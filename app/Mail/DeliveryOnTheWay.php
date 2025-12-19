26b82a086b51bf0ec36dda88093af709eb0e074a
Alogo Situmorang <aekmohop@gmail.com>
2025-12-19 23:12:31 +0700

    ``` feat: Add email notification mailables for payment receipt and delivery tracking
    Add PaymentReceipt mailable to send payment confirmation emails with rental property and dynamic subject line including rental code. Add DeliveryOnTheWay mailable to notify customers when their order is out for delivery with rental property and dynamic subject line including rental code.
    ```

---
+++ app/Mail/DeliveryOnTheWay.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Rental;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class DeliveryOnTheWay extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $rental;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Rental $rental)
+    {
+        $this->rental = $rental;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Pesanan Anda Sedang Diantar! - ' . $this->rental->kode)
+                    ->view('emails.delivery_on_the_way');
+    }
+}
